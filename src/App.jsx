d7c792cc4267eea7e14bcfd107d16178c5b701b7
Yivan2021 <manuel.rodriguez@softwareestrategico.com>
2022-05-02 09:07:42 -0500

    create

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Body } from "./Components/Body";
+import { Content } from "./Components/Content/Content";
+import { NavBar } from "./Components/NavBar";
+
+function App() {
+  return (
+          <>
+            <NavBar/>
+            <Content/>
+            <Content/>
+            <NavBar/>
+          </>
+  )
+}
+export default App;
+
+// <div className="" >
+//         <Body
+//         name="pepito"
+//          cel={5454545}
+//          data={{nombre:'algo'}}
+//          />
+//     </div>
---
+++ src/Components/Content/Content.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './content.css';
+
+export const Content = () => {
+    return (
+        <>
+         <div className="card " >
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="card-link">Card link</a>
+                    <a href="#" className="card-link">Another link</a>
+                </div>
+            </div>
+            <div className="card formato" >
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="card-link">Card link</a>
+                    <a href="#" className="card-link">Another link</a>
+                </div>
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit numquam accusamus, quia officia consequatur alias illo? Ipsum tempore recusandae velit quasi sapiente officia quo alias excepturi sequi, aliquid maxime! Veniam.
+            </p>
+        </>
+    )
+}
